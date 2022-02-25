8261174a633d66ddb6da07f0122b177b1b592f0b
Devendra <devendrakumart8@gmail.com>
2022-02-25 21:51:42 +0530

    navbar added

---
+++ navbar.js
@@ -0,0 +1,53 @@
+function navbar(){
+    return `
+    <div id="navbar_container">
+        <div id="nav_left">
+            <div id="nav_logo">
+                <img src="https://i.ibb.co/4tn5YV5/navporter-logo.png" alt="">
+            </div>
+            <div >
+                <p id="nav_heading">India's Largest Marketplace for Intracity Logistics</p>
+            </div>
+        </div>
+        <div id="nav_right">
+            <div id="drive-us">
+                <div id="drive-img">
+                    <img id="drive-icon" src="https://d35ppnqksufxmo.cloudfront.net/frontend_website/9e4ccae9c5a1b669d8fa2648db220c3f.svg" alt="">
+                </div>
+                <div id="drive-heading">
+                    <p id="drive_title">DRIVE WITH US</p>
+                    <p id="drive_desc">turn kms into money</p>
+                </div>
+                <div id="join-us">
+                    <p >JOIN US</p>
+                </div>
+            </div>
+            <div id="contact_info">
+                <div>
+                    <img src="https://i.ibb.co/P4n85zK/call-icon.png" alt="" id="call-icon">
+                </div>
+                <div>
+                    <p id="contact_num">4410-4410</p>
+                    <p id="city-code">add your city code</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="navbar_container2">
+        <div id="nav2_left">
+            <p>CITY TEMPO</p>
+            <p>PACKERS & MOVERS</p>
+            <p>DRIVER PARTNERS</p>
+            <p>FOR ENTERPRISE</p>
+        </div>
+        <div id="nav2_right">
+            <p>Track Order</p>
+            <p>Support</p>
+        </div>
+    </div>
+    
+    `
+}
+
+export default navbar;
