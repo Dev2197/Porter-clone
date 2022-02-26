6a24bc9ca03651d851d80ca0e15692fb8fce5716
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-02-26 22:31:23 +0530

    Added new files

---
+++ City Tempo_Landing page/city_tempo.js
@@ -0,0 +1,32 @@
+
+import navbar from "./components/header.js";
+
+let nav = document.getElementById('main');
+nav.innerHTML = navbar();
+
+
+import footer from "./components/footer.js";
+
+let foot = document.getElementById('end');
+foot.innerHTML = footer();
+
+
+
+let phone_inside = document.querySelector(".phone_inside");
+    let phone_images = ["https://d35ppnqksufxmo.cloudfront.net/frontend_website/dc62acbd47448f7408a42cafd2373ff4.jpg",
+                        "https://d35ppnqksufxmo.cloudfront.net/frontend_website/06836ba0537d528a5e6d67162486c5dd.png",
+                        "https://d35ppnqksufxmo.cloudfront.net/frontend_website/f2aa8b1ddc884cb99c166ef3f0126d11.jpg",
+                        "https://d35ppnqksufxmo.cloudfront.net/frontend_website/f5523c6e547962ddd0c9e4a671aab308.jpg"
+                    ];
+
+    
+    var i = 0;
+
+    setInterval(function(){
+        phone_inside.src = phone_images[i];
+        
+        if(i == 3){
+            i = 0;
+        }
+        i++;
+    },2000);
---
+++ City Tempo_Landing page/components/footer.js
@@ -0,0 +1,95 @@
+function footer(){
+    return `
+    <div id="footer_container">
+        <div id="footer1">
+            <div id="company_info">
+                <img src="https://i.ibb.co/bH4LLYd/porter-footer.png" alt="" id="porter_logo">
+                <p id="logo_desc">Send anything, anywhere, anytime</p>
+            </div>
+
+            <div>
+                <p class="footer_title">Company</p>
+                <p class="footer_subtitle">About us </p>
+                <p class="footer_subtitle">Offers</p>
+                <p class="footer_subtitle">Careers</p>
+                <p class="footer_subtitle">Blog</p>
+            </div>
+
+            <div>
+                <p class="footer_title" >Services</p>
+                <p class="footer_subtitle">City Tempo</p>
+                <p class="footer_subtitle">For Enterprise</p>
+                <p class="footer_subtitle">Packers & Movers</p>
+            </div>
+
+            <div>
+                <p class="footer_title">Support</p>
+                <p class="footer_subtitle">Contact Us</p>
+                <p class="footer_subtitle">Privacy Policy</p>
+                <p class="footer_subtitle">Terms Of Service</p>
+                <p class="footer_subtitle">Insurance of FAQs</p>
+            </div>
+
+            <div id="playstore">
+                <div>
+                    <img src="https://lh3.googleusercontent.com/cjsqrWQKJQp9RFO7-hJ9AfpKzbUb_Y84vXfjlP0iRHBvladwAfXih984olktDhPnFqyZ0nu9A5jvFwOEQPXzv7hr3ce3QVsLN8kQ2Ao=s0" alt="" class="playstore_logo">
+                </div>
+                <div>
+                    <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="" class="playstore_logo">
+                </div>
+            </div>
+        </div>
+
+        
+        <div id="footer2_head">
+            <p class="footer_title" id="here">We are here</p>
+            <div id="footer2">
+                <div style="margin-right: 50px;">
+                    <p class="footer_subtitle">Mumbai</p>
+                    <p class="footer_subtitle">Ahmedabad</p>
+                    <p class="footer_subtitle">Lucknow</p>
+                </div>
+                <div>
+                    <p class="footer_subtitle">Delhi NCR</p>
+                    <p class="footer_subtitle">Jaipur</p>
+                    <p class="footer_subtitle">Coimbatore</p>
+                </div>
+                <div>
+                    <p class="footer_subtitle">Bangalore</p>
+                    <p class="footer_subtitle">Pune</p>
+                    <p class="footer_subtitle">Indore</p>
+                </div>
+                <div>
+                    <p class="footer_subtitle">Hyderabad</p>
+                    <p class="footer_subtitle">Kolkata</p>
+                    <p class="footer_subtitle">Nagpur</p>
+                </div>
+                <div>
+                    <p class="footer_subtitle">Chennai</p>
+                    <p class="footer_subtitle">Surat</p>
+                    <p class="footer_subtitle">Chandigarh</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer3">
+            <div id="social_media_icons">
+                <img src="https://i.ibb.co/59qXTcc/instagram-icon.png" alt="">
+                <img src="https://i.ibb.co/9rrr6DR/facebook-icon.png" alt="">
+                <img src="https://i.ibb.co/2K4Vb8d/linkedin-icon.png" alt="">
+                <img src="https://i.ibb.co/p04TcFf/twitter-icon.png" alt="">
+                <img src="https://i.ibb.co/KwpyVGQ/youtube-icon.png" alt="">
+            </div>
+
+            <div id="copy_right">
+                <p>©2021 SmartShift Logistics Solutions Pvt. Ltd.</p>
+                <div id="vertical_line"></div>
+                <p>CIN: U74999MH2014PTC306120</p>
+            </div>
+        </div>
+    </div>
+    
+    `
+}
+
+export default footer;
